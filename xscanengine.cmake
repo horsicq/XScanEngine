478aabf7437a88ffff6a189b1e584478d4f3c5f5
horsicq <horsicq@gmail.com>
2024-12-03 00:59:09 +0100

    Update file(s): 2024-12-03

--- xscanengine.cmake
+++ xscanengine.cmake
@@ -4,8 +4,12 @@ if (NOT DEFINED XFORMATS_SOURCES)
     include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
     set(XSCANENGINE_SOURCES ${XSCANENGINE_SOURCES} ${XFORMATS_SOURCES})
 endif()
+if (NOT DEFINED XEXTRACTOR_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../XExtractor/xextractor.cmake)
+    set(XSCANENGINE_SOURCES ${XSCANENGINE_SOURCES} ${XEXTRACTOR_SOURCES})
+endif()
 
-include(${CMAKE_CURRENT_LIST_DIR}/../XExtractor/xextractor.cmake)
+
 include(${CMAKE_CURRENT_LIST_DIR}/../XDEX/xdex.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XPDF/xpdf.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XArchive/xarchives.cmake)
@@ -14,12 +18,12 @@ include(${CMAKE_CURRENT_LIST_DIR}/../XOptions/xoptions.cmake)
 set(XSCANENGINE_SOURCES
     ${XSCANENGINE_SOURCES}
     ${XFORMATS_SOURCES}
+    ${XEXTRACTOR_SOURCES}
     ${XDEX_SOURCES}
     ${XPDF_SOURCES}
     ${XARCHIVES_SOURCES}
     ${XFORMATS_SOURCES}
     ${XOPTIONS_SOURCES}
-    ${XEXTRACTOR_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xscanengine.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xscanengine.h
     ${CMAKE_CURRENT_LIST_DIR}/scanitem.cpp
