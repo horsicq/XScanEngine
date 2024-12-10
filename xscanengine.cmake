d9cbe20c14644a58305253b9b794564bfe619757
horsicq <horsicq@gmail.com>
2024-12-10 01:24:58 +0100

    Update file(s): 2024-12-10

--- xscanengine.cmake
+++ xscanengine.cmake
@@ -12,8 +12,11 @@ if (NOT DEFINED XDEX_SOURCES)
     include(${CMAKE_CURRENT_LIST_DIR}/../XDEX/xdex.cmake)
     set(XSCANENGINE_SOURCES ${XSCANENGINE_SOURCES} ${XDEX_SOURCES})
 endif()
+if (NOT DEFINED XPDF_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../XPDF/xpdf.cmake)
+    set(XSCANENGINE_SOURCES ${XSCANENGINE_SOURCES} ${XPDF_SOURCES})
+endif()
 
-include(${CMAKE_CURRENT_LIST_DIR}/../XPDF/xpdf.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XArchive/xarchives.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XOptions/xoptions.cmake)
 
