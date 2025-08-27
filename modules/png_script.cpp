5db8a11ac2d71f3488bd8fc4567912b412f745b8
horsicq <horsicq@gmail.com>
2025-08-27 20:42:16 +0200

    Add new file(s): 2025-08-27

---
+++ modules/png_script.cpp
@@ -0,0 +1,93 @@
+/* Copyright (c) 2019-2025 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "png_script.h"
+
+PNG_Script::PNG_Script(XPNG *pPNG, XBinary::FILEPART filePart, OPTIONS *pOptions, XBinary::PDSTRUCT *pPdStruct) : Image_Script(pPNG, filePart, pOptions, pPdStruct)
+{
+    g_pPNG = pPNG;
+    g_ihdr = g_pPNG->getIHDR();
+}
+
+quint32 PNG_Script::getWidth()
+{
+    return g_ihdr.nWidth;
+}
+
+quint32 PNG_Script::getHeight()
+{
+    return g_ihdr.nHeight;
+}
+
+quint8 PNG_Script::getBitDepth()
+{
+    return g_ihdr.nBitDepth;
+}
+
+quint8 PNG_Script::getColorType()
+{
+    return g_ihdr.nColorType;
+}
+
+quint8 PNG_Script::getCompression()
+{
+    return g_ihdr.nCompression;
+}
+
+quint8 PNG_Script::getFilter()
+{
+    return g_ihdr.nFilter;
+}
+
+quint8 PNG_Script::getInterlace()
+{
+    return g_ihdr.nInterlace;
+}
+
+bool PNG_Script::isChunkPresent(const QString &sChunkType)
+{
+    // TODO: Implement chunk enumeration and checking
+    // This would require adding a getChunks() method to XPNG class
+    Q_UNUSED(sChunkType)
+    return false;
+}
+
+qint32 PNG_Script::getNumberOfChunks()
+{
+    // TODO: Implement chunk counting
+    // This would require adding a getChunks() method to XPNG class
+    return 0;
+}
+
+QString PNG_Script::getChunkName(qint32 nIndex)
+{
+    // TODO: Implement chunk name retrieval
+    // This would require adding a getChunks() method to XPNG class
+    Q_UNUSED(nIndex)
+    return "";
+}
+
+quint32 PNG_Script::getChunkSize(qint32 nIndex)
+{
+    // TODO: Implement chunk size retrieval
+    // This would require adding a getChunks() method to XPNG class
+    Q_UNUSED(nIndex)
+    return 0;
+}
