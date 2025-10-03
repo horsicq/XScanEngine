3e47b8fd0754a0103b9c329b471aea9b33e8268a
horsicq <horsicq@gmail.com>
2025-10-03 11:26:44 +0200

    Add new file(s): 2025-10-03

---
+++ modules/atarist_script.h
@@ -0,0 +1,43 @@
+/* Copyright (c) 2019-2025 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef ATARIST_SCRIPT_H
+#define ATARIST_SCRIPT_H
+
+#include "xatarist.h"
+#include "binary_script.h"
+
+class AtariST_Script : public Binary_Script {
+    Q_OBJECT
+
+public:
+    explicit AtariST_Script(XAtariST *pAtariST, XBinary::FILEPART filePart, OPTIONS *pOptions, XBinary::PDSTRUCT *pPdStruct);
+
+private:
+    XAtariST *g_pAtariST;
+
+protected:
+    XAtariST *getAtariST() const
+    {
+        return g_pAtariST;
+    }
+};
+
+#endif  // ATARIST_SCRIPT_H
